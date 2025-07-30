b1647418226ae43b889e338aefb2e00a58c1f9f7
Jon Martins <jond31201@gmail.com>
2025-07-29 21:20:22 -0400

    Create find_1kg_overlap.py
    Python script made in order to find which genomes from the 1000 Genomes Project overlap with HGSVC and HPRC

---
+++ find_1kg_overlap.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+HGSVC_file = "HGSVC2_sample_list.txt"
+HPRC_file = "HPRC_PacBio_HiFi.file.index.csv"
+ONEKG_file = "20130606_sample_info.txt"
+
+onekg = pd.read_csv(ONEKG_file, sep='\t')
+onekg_samples = set(onekg['Sample'])
+
+hgsvc = pd.read_csv(HGSVC_file, sep='\t', header=None, usecols=[1], names=["sample"])
+hgsvc_samples = set(hgsvc['sample'])
+
+hprc = pd.read_csv(HPRC_file)
+hprc_samples = set(hprc['sample_ID'])
+
+common_hgsvc = sorted(onekg_samples.intersection(hgsvc_samples))
+common_hprc = sorted(onekg_samples.intersection(hprc_samples))
+
+with open("overlap_1KG_HGSVC.txt", "w") as f:
+    f.write("\n".join(common_hgsvc))
+
+with open("overlap_1KG_HPRC.txt", "w") as f:
+    f.write("\n".join(common_hprc))
+
+print(f"Found {len(common_hgsvc)} overlapping samples with HGSVC")
+print(f"Found {len(common_hprc)} overlapping samples with HPRC")
